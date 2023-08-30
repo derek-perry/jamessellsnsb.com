e06baa7929dda24de902b5484464dc7c63aad167
DP <dp@dp.design>
2023-08-30 01:53:40 -0400

    Add Contact Info
    Contact information was added to the homepage and footer with initial styling/layout and without logos.

--- components/PageFooter/index.tsx
+++ components/PageFooter/index.tsx
@@ -12,6 +12,10 @@ const PageFooter = (): JSX.Element => {
             <h1 className="font-light text-5xl">James Sells NSB</h1>
           </LinkInternal>
           <div className="flex flex-row flex-wrap gap-x-8 gap-y-2 text-center items-center justify-center">
+              <p>Email: <LinkExternal className="!text-white focus:!text-slate-400 hover:!text-slate-300 no-underline break-all" href="mailto:contact@jamessellsnsb.com" title="Contact James Cumiskey via Email at Contact@JamesSellsNSB.com">Contact@JamesSellsNSB.com</LinkExternal></p>
+              <p>Phone: <LinkExternal className="!text-white focus:!text-slate-400 hover:!text-slate-300 no-underline break-all" href="tel:3864449116" title="Contact James Cumiskey via Phone at 386-444-9116">386-444-9116</LinkExternal></p>
+          </div>
+          <div className="flex flex-row flex-wrap gap-x-8 gap-y-2 text-center items-center justify-center">
               <p> © {currentYear} <LinkInternal className="!text-white focus:!text-slate-400 hover:!text-slate-300 no-underline" href="" title="James Cumiskey">James Cumiskey</LinkInternal></p>
               <LinkInternal className="!text-white focus:!text-slate-400 hover:!text-slate-300" href="privacy" title="Privacy Policy">Privacy Policy</LinkInternal>
           </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import SiteHead from "../components/SiteHead"
 import PageFooter from "../components/PageFooter"
+import LinkExternal from "../components/Links/LinkExternal"
 import Image from 'next/image'
 
 const homePage: NextPage = () => {
@@ -19,9 +20,19 @@ const homePage: NextPage = () => {
           className="shadow-lg"
         />
 
-        <p className="mt-10 mb-20 font-normal text-4xl">
+        <p className="mt-10 mb-10 font-normal text-4xl">
           Realtor ®
         </p>
+
+        <div className="mb-10 text-xl flex flex-row flex-wrap gap-x-8 gap-y-2 text-center items-center justify-center">
+            <p>Email: <LinkExternal className="no-underline break-all" href="mailto:contact@jamessellsnsb.com" title="Contact James Cumiskey via Email at Contact@JamesSellsNSB.com">Contact@JamesSellsNSB.com</LinkExternal></p>
+            <p>Phone: <LinkExternal className="no-underline break-all" href="tel:3864449116" title="Contact James Cumiskey via Phone at 386-444-9116">386-444-9116</LinkExternal></p>
+        </div>
+
+        <div className="mb-20 text-xl flex flex-row flex-wrap gap-x-8 gap-y-2 text-center items-center justify-center">
+            <p>TikTok: <LinkExternal className="no-underline break-all" href="https://tiktok.com/@James.CumiskeySales" title="Follow James Cumiskey on TikTok at James.CumiskeySales">James.CumiskeySales</LinkExternal></p>
+            <p>Instagram: <LinkExternal className="no-underline break-all" href="https://instagram.com/JamesRDCumiskeyRealEstateSales" title="Follow James Cumiskey on Instagram at JamesRDCumiskeyRealEstateSales">JamesRDCumiskeyRealEstateSales</LinkExternal></p>
+        </div>
       </main>
 
       <PageFooter />
